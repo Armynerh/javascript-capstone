08b2c64743eacc27648903ee96e53da4f7021982
Danny-imo <dannyusua@gmail.com>
2023-08-11 09:33:28 +0100

    Fix Errors

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,7 +1,7 @@
 const updateItemCount = (countElement, itemListElement) => {
-    const itemCount = itemListElement.childElementCount;
-    countElement.innerText = `(${itemCount})`;
-    return itemCount;
-  };
-  
-  export default updateItemCount;
+  const itemCount = itemListElement.childElementCount;
+  countElement.innerText = `(${itemCount})`;
+  return itemCount;
+};
+
+export default updateItemCount;
